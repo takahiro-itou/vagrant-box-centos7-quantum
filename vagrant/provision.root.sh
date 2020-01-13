6eaded83bcccd34745d66b600b387625807c308b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-01-13 11:31:40 +0900

    パッケージをインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,3 +6,6 @@ sudo  timedatectl  set-timezone Asia/Tokyo
 
 sudo  yum  update -y
 sudo  yum  groupinstall -y  "Base"  "Development Tools"
+
+sudo  rpm  -Uvh  https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+sudo  yum  install -y  dotnet-sdk-3.1
