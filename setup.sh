84af947ac38cc8e6175af5bd8f479cd16debd815
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-01-13 11:24:32 +0900

    ボックス化のスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash  -xue
+
+boxFile='centos7-quantum.box'
+boxName='centos7-quantum'
+
+# Account Name of Vagrant Cloud.
+vcUser='takahiro-itou'
+
+pushd  vagrant
+
+vagrant  destroy -f  || exit $?
+vagrant  up          || exit $?
+vagrant  halt
+
+vagrant  package   --output ../${boxFile}  default
+
+popd
+set  +x
+
+cat  << _EOF_  1>&2
+To add package into box list, run:
+_EOF_
+
+cat  << _EOF_
+vagrant  box  add  --name ${vcUser}/${boxName}  ${boxFile}
+_EOF_
